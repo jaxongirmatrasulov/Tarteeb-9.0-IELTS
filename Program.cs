cddb05895cf93a56624c93a145437a323f8102ac
jaxongirmatrasulov <jahongirmatrasulov2000@gmail.com>
2024-06-19 18:58:12 +0500

    metod

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// class Program
+// {
+//     static void MyMethod(string name)
+//     {
+//         Console.WriteLine(name + "Matrasulov");
+//     }
+// static void Main(string[] args)
+//     {
+//         MyMethod("Jaxongir ");
+//         MyMethod("Abror ");
+//         MyMethod("Xushnud ");
+//     }
+// }
+
+
+
+class Program
+{
+    static int MyMethod(int x, int y, int a, int d)
+    {
+        return x + y * a + d;
+    }
+    static void Main(string[] args)
+    {
+        int k = MyMethod(2, 5, 4, 3);
+        Console.WriteLine(k);
+    }
+}
